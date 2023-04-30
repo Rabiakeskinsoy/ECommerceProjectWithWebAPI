5a50fc44c6e9f4b4e380f4ce50d18727864ba06f
Rabiakeskinsoy <rabiakeskinsoy2727@gmail.com>
2023-04-30 15:09:06 +0300

    Dtos oluştuldu

---
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete.BaseEntities;
+using System;
+
+namespace Entities.Dtos.UserDtos
+{
+    /// <summary>
+    /// Bu dto Müşteriye göstermek için oluşturuyoruz.
+    /// </summary>
+    public class UserDetailDto:AuditableEntity
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
