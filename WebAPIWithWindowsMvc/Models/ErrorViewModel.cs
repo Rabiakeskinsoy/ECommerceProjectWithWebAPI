affc383e6134ff8c28f9da042a40ba02da506126
Rabiakeskinsoy <rabiakeskinsoy2727@gmail.com>
2023-04-30 13:16:36 +0300

    Commerce 1

---
+++ WebAPIWithWindowsMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAPIWithWindowsMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
